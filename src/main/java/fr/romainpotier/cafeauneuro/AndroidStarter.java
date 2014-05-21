97baa2d2cc259acb194f4635ed9797abb6d05cf9
romain_potier <rpotier.prestataire@voyage-sncf.com>
2014-05-21 11:18:56 +0200

    Initial Import

---
+++ src/main/java/fr/romainpotier/cafeauneuro/AndroidStarter.java
@@ -0,0 +1,55 @@
+package fr.romainpotier.cafeauneuro;
+
+import java.util.List;
+
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.rest.RestService;
+
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import fr.romainpotier.cafeauneuro.beans.Movie;
+import fr.romainpotier.cafeauneuro.service.MoviesService;
+
+/**
+ * Simple activity for testing REST webservice
+ * 
+ * @author Romain Potier
+ */
+@EActivity(R.layout.activity_main)
+public class AndroidStarter extends Activity {
+
+    @ViewById
+    TextView textView;
+
+    @ViewById
+    ListView listView;
+
+    @RestService
+    MoviesService moviesService;
+
+    @Click
+    void callRestService() {
+        callMoviesService();
+    }
+
+    @Background
+    void callMoviesService() {
+        // Call service
+        List<Movie> movies = moviesService.getMovies();
+        updateListView(movies);
+    }
+
+    @UiThread
+    void updateListView(final List<Movie> movies) {
+        // Create custom adapter
+        final ArrayAdapter<Movie> arrayAdapter = new ArrayAdapter<Movie>(this, android.R.layout.simple_list_item_1, movies);
+        listView.setAdapter(arrayAdapter);
+    }
+
+}
---
+++ src/main/java/fr/romainpotier/cafeauneuro/service/MoviesService.java
@@ -0,0 +1,21 @@
+package fr.romainpotier.cafeauneuro.service;
+
+import java.util.List;
+
+import org.androidannotations.annotations.rest.Get;
+import org.androidannotations.annotations.rest.Rest;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+
+import fr.romainpotier.cafeauneuro.beans.Movie;
+
+/**
+ * REST webservice example
+ * 
+ * @author Romain Potier
+ */
+@Rest(rootUrl = "http://176.31.101.144:99", converters = { MappingJacksonHttpMessageConverter.class })
+public interface MoviesService {
+
+    @Get("/movies")
+    List<Movie> getMovies();
+}
